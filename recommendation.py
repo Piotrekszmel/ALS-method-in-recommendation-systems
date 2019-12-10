d4e1ae8feaa65be3ac895b7f9475f54f374cd91d
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-12-10 21:45:00 +0100

    create new dataframe

--- recommendation.py
+++ recommendation.py
@@ -3,19 +3,68 @@ import pandas as pd
 from utils import recommendation, create_matrix, add_rows
 import operator
 
-LR = 0.1
+LR = 0.4
 D =  3
-N = 300000
+N = 497800
 L = 10
 DATA_PATH = "dataset/amazon-meta.txt"
 
+data = pd.read_csv("amazon_data.csv", index_col=False)
+
+title1 = data.loc[data["Title"] == "Make a Wish (Holiday Greetings Cards)"]
+title2 = data.loc[data["Title"] == "The Man Who Sold the World"]
+title3 = data.loc[data["Title"] == "The Three Stooges - Spook Louder"]
+title4 = data.loc[data["Title"] == "True Grits : The Southern Foods Mail-Order Catalog "]
+title5 = data.loc[data["Title"] == "Quick Toning: Thighs of Steel "]
+title6 = data.loc[data["Title"] == "Simply Red - Live in London"]
+title7 = data.loc[data["Title"] == "The Kama Sutra of Vatsyayana"]
+
+idxs = {}
+titles = [title1, title2, title5, title6, title7]
+
+for title in titles:
+    for idx in title["Id"]:
+        if idx in idxs:
+            idxs[idx] += 1
+        else: 
+            idxs[idx] = 1
+
+sorted_idx = sorted(idxs.items(), key=operator.itemgetter(1))     
+top_idx = sorted_idx[-10:]
+
+R = np.zeros(shape=(10, 5))
+for i, title in enumerate(titles):
+    indexes = [id for id in title["Id"]]
+    for j, idx in enumerate(top_idx):
+        if idx[0] in indexes:
+            R[j, i] = title["Ratings"].loc[title["Id"] == idx[0]][-1:]
+
+U = np.zeros(shape=(D, R.shape[0]))
+P = np.zeros(shape=(D, R.shape[1]))
+
+
+R, U, P = recommendation(R, U, P, L, LR, D)
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 df = pd.DataFrame(columns=['Group', 'Title', 'Id', 'Ratings'])
 df = add_rows(df, DATA_PATH, N, "Book")
+print("Indexing...")
 users = set(df["Id"])
 df["index"] = range(0, len(df))
+print("\nSaving...")
+df.to_csv("amazon_data.csv")
 
-df.to_csv()
-"""
 R, U, P, User_id, Product_id, df = create_matrix(DATA_PATH, 10000, 3, ["Book", " DVD", "Video", "Music"])
 
 df.to_csv(index=False)
--- utils.py
+++ utils.py
@@ -13,7 +13,8 @@ def parse_text(size, data_path, category):
     for row in text:
         row = row.replace("\n", "")
         
-        if len(set(titles)) == size:
+        if len(titles) == size:
+            print("\nbreaking...")
             break
         
         if 'group: ' in row and any(cat in row for cat in category):
@@ -34,13 +35,16 @@ def parse_text(size, data_path, category):
             final_list.append([product_type, title, customer_id, rating])
             titles.append(title)
             rating = 0
-            product_flag = 0
+            product_flag = 0,
+            if len(titles) % 100000 == 0:
+                print(len(titles))
          
     return final_list
 
 
 def add_rows(df, data_path, size, category):
     data = parse_text(size, data_path, category)
+    print("appending...\n")
     for row in data:
         df = df.append(pd.Series(row, index=df.columns), ignore_index=True)
     print('DataFrame Updated!')
@@ -88,8 +92,8 @@ def objective_function(R_up, U, P, lr):
     return value
 
 
-def recommendation(data_path, d, n, l, lr, category):   
-    R, U, P, User_id, Product_id, df = create_matrix(data_path, n, d, category)
+def recommendation(R, U, P, l, lr, d):   
+    #R, U, P, User_id, Product_id, df = create_matrix(data_path, n, d, category)
     
     R_up = create_R_up(R)
     U = np.random.uniform(0, 1, (U.shape[0], U.shape[1]))
@@ -122,5 +126,52 @@ def recommendation(data_path, d, n, l, lr, category):
         else:
             print(value)
     print("\n")
-    return R, U, P, User_id, Product_id, df
+    return R, U, P
 
+
+"""
+def GEPP(A, b, doPricing = False):
+    '''
+    Gaussian elimination with partial pivoting.
+    
+    input: A is an n x n numpy matrix
+           b is an n x 1 numpy array
+    output: x is the solution of Ax=b 
+            with the entries permuted in 
+            accordance with the pivoting 
+            done by the algorithm
+    post-condition: A and b have been modified.
+    '''
+    n = len(A)
+    if b.size != n:
+        raise ValueError("Invalid argument: incompatible sizes between"+
+                         "A & b.", b.size, n)
+    # k represents the current pivot row. Since GE traverses the matrix in the 
+    # upper right triangle, we also use k for indicating the k-th diagonal 
+    # column index.
+    
+    # Elimination
+    for k in range(n-1):
+        if doPricing:
+            # Pivot
+            maxindex = abs(A[k:,k]).argmax() + k
+            if A[maxindex, k] == 0:
+                raise ValueError("Matrix is singular.")
+            # Swap
+            if maxindex != k:
+                A[[k,maxindex]] = A[[maxindex, k]]
+                b[[k,maxindex]] = b[[maxindex, k]]
+        else:
+            if A[k, k] == 0:
+                raise ValueError("Pivot element is zero. Try setting doPricing to True.")
+        #Eliminate
+        for row in range(k+1, n):
+            multiplier = A[row,k]/A[k,k]
+            A[row, k:] = A[row, k:] - multiplier*A[k, k:]
+            b[row] = b[row] - multiplier*b[k]
+    # Back Substitution
+    x = np.zeros(n)
+    for k in range(n-1, -1, -1):
+        x[k] = (b[k] - np.dot(A[k,k+1:],x[k+1:]))/A[k,k]
+    return np.asmatrix(x).T
+"""
