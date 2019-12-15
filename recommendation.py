8b112cae5ea29b87cd417606fe338bf2448eee34
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-12-15 17:50:54 +0100

    create result images

--- recommendation.py
+++ recommendation.py
@@ -1,124 +1,140 @@
 import numpy as np
 import pandas as pd
-from utils import recommendation, create_matrix, add_rows, create_matrices
+import matplotlib.pyplot as plt
 import operator
+import copy
+from random import randrange
+
+from utils import recommendation, create_matrix, add_rows, create_matrices
+
 
 LR = 0.2
-D = 10
+D = 3
+N = 1000000
+L = 100
+LR = [0.1, 0.2, 0.3]
+D = [8, 10, 12]  
+DATA_PATH = "dataset/amazon-meta.txt"
+
+values = []
+indexes = []
+reviews = []
+rows = []
+diffs = np.zeros(shape=(len(D), len(LR)))
+
+for y, dim in enumerate(D):
+    for x, learning_rate in enumerate(LR):
+        diff = 0 
+        
+        R, U, P = create_matrices("Book", dim,  300, 1100, "amazon_data.csv")
+        R_t = copy.deepcopy(R)
+
+        for i in range(R.shape[0]):
+            index = randrange(0, 9)
+            if R[i][index] != 0.0:
+                rows.append(i)
+                indexes.append(index)
+                reviews.append(R[i][index])
+                R[i][index] = 0.0
+
+        R, U, P, cost = recommendation(R, U, P, L, learning_rate, dim, 0)
+        R = np.array(R)
+        
+        values.append(cost)
+
+        R_ = np.array(np.matmul(U.T, P))
+        
+        U = np.array(U)
+        P = np.array(P)
+
+        for i in range(len(indexes)):
+                diff += abs(R_t[rows[i], indexes[i]] - int(R_[rows[i], indexes[i]]))
+        diffs[x][y] = float(diff)
+ 
+for i in range(0, len(D)):
+    plt.plot([dim for dim in D], [diff for diff in diffs[i]], label="LR = " + str(LR[i]))
+plt.xlabel("d")
+plt.ylabel("absolute difference between R and R~")
+plt.legend()
+plt.savefig("1200_1100")
+plt.show()
+
+
+
+
+""" 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import operator
+import copy
+from random import randrange
+
+from utils import recommendation, create_matrix, add_rows, create_matrices
+
+
+LR = 0.2
+D = 3
 N = 1000000
 L = 100
 DATA_PATH = "dataset/amazon-meta.txt"
 
-data = pd.read_csv("amazon_data.csv", index_col=False)
+LR = [0.1, 0.2, 0.3]
+D = [8, 10, 12]  
+indexes = []
+reviews = []
+rows = []
+diffs = np.zeros(shape=(len(D), len(LR)))
+values = []
 
-title1 = data.loc[data["Title"] == "Make a Wish (Holiday Greetings Cards)"]
-title2 = data.loc[data["Title"] == "The Man Who Sold the World"]
-title3 = data.loc[data["Title"] == "The Three Stooges - Spook Louder"]
-title4 = data.loc[data["Title"] == "True Grits : The Southern Foods Mail-Order Catalog"]
-title5 = data.loc[data["Title"] == "Quick Toning: Thighs of Steel"]
-title6 = data.loc[data["Title"] == "Simply Red - Live in London"]
-title7 = data.loc[data["Title"] == "The Kama Sutra of Vatsyayana"]
-
-LR =[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-learning_rates = [0.5]
-
-for learning_rate in learning_rates:
-    diff = 0
-    #titles = [title1, title2, title6, title5, title3, title4]
-    
-    R, U, P = create_matrices("Book", D, 30, 30, "amazon_data.csv")
-
-    R, U, P = recommendation(R, U, P, L, learning_rate, D, 1)
-    R = np.array(R)
-
-    R_ = np.array(np.matmul(U.T, P))
-    
-    U = np.array(U)
-    P = np.array(P)
-
-
-    for i in range(R.shape[0]):
-        for j in range(R.shape[1]):
-            diff += abs(R[i][j] - R_[i][j])
+for y, dim in enumerate(D):
+    for x, learning_rate in enumerate(LR):
+        diff = 0 
         
-    print("\ndiff = ", diff, "\nlr = ", learning_rate)
+        R, U, P = create_matrices("Book", dim,  1500, 1200, "amazon_data.csv")
+        
+        R_t = copy.deepcopy(R)
 
+        for i in range(R.shape[0]):
+            index = randrange(0, 9)
+            if R[i][index] != 0.0:
+                rows.append(i)
+                indexes.append(index)
+                reviews.append(R[i][index])
+                R[i][index] = 0.0
 
-    for val in R_:
-        for v in val:
-            print(int(v), end=" ")
-        print()
-    print("\n")
-    """
-    for val in U:
-        for v in val:
-            print(v, end=" ")
-        print()
-    print("\n")
+        
+        R, U, P, cost = recommendation(R, U, P, L, learning_rate, dim, 0)
+        R = np.array(R)
+        
+        values.append(cost)
+
+        R_ = np.array(np.matmul(U.T, P))
+        
+        U = np.array(U)
+        P = np.array(P)
+        
+        for i in range(len(indexes)):
+                diff += abs(R_t[rows[i], indexes[i]] - int(R_[rows[i], indexes[i]]))
+                
+        diffs[x][y] = float(diff)
+        
+        
+        
     
-    for val in P:
-        for v in val:
-            print(v, end="   ")
-        print()
-    print("\n")
-    """
-    for val in R:
-        for v in val:
-            print(int(v), end=" ")
-        print()
-    print("\n")
-"""
+for i in range(0, len(D)):
+    plt.plot([dim for dim in D], [diff for diff in diffs[i]], label="LR = " + str(LR[i]))
 
-for val in R:
-    for v in val:
-        print(int(v), end=" ")
-    print()
-
-print("\n")
-"""
-"""
-df = pd.DataFrame(columns=['Group', 'Title', 'Id', 'Ratings'])
-df = add_rows(df, DATA_PATH, N, ["Book", " DVD", "Video", "Music"])
-print("Indexing...")
-users = set(df["Id"])
-df["index"] = range(0, len(df))
-print("\nSaving...")
-df.to_csv("amazon_data2.csv")
-"""
-"""
-R, U, P, User_id, Product_id, df = create_matrix(DATA_PATH, 10000, 3, ["Book", " DVD", "Video", "Music"])
-
-df.to_csv(index=False)
+for i in range(0, len(D)):
+    plt.plot([iteration for iteration in range(L)], [c for c in values[i]], label="LR = " + str(LR[i]) + "D = " + str(D[i]))
+plt.xlabel("Iteration")
+plt.ylabel("Objective Function value")
+plt.legend()
+plt.savefig("100_10_cost")
+plt.show()
 
 
-R, U, P, User_id, Product_id, df = recommendation(DATA_PATH, D, N, L, LR, ["Book", " DVD", "Video", "Music"])
 
-R_ = np.array(np.matmul(U.T, P))
 
-print("XDDDDDDDDDDDDDDDDDDDDDDDDD")
-for val in R_:
-    for v in val:
-        print(int(v), end=" ")
-    print()
-print("XDDDDDDDDDDDDDDDDDDDDDDDDD")
-print("\n")
 
-for val in R:
-    for v in val:
-        print(v, end=" ")
-    print()
-
-print("\n")
-
-for val in U:
-    for v in val:
-        print(v, end=" ")
-    print()
-
-print("\n")
-
-for val in P:
-    for v in val:
-        print(v, end=" ")
-    print()
 """
